3ba561f2e49e49412bc6e980d67091840be16d97
Andrei Kovtun <andreikovtun@MacBook-Air-Andrei.local>
2022-11-10 12:04:23 +0300

    first commit

---
+++ ConsoleInterface/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using ProcessingStringNamespace;
+using static System.Net.Mime.MediaTypeNames;
+
+//Console.WriteLine("Введите первое слово");
+//string? a = Console.ReadLine();
+//Console.WriteLine("Введите второе слово");
+//string? b = Console.ReadLine();
+////StringBuilder test = new StringBuilder("jrte43uu64te 95 12", 30);
+////Console.WriteLine(ProcessingString.SumOfNumbersInString(test.ToString()));
+//string ans = ProcessingString.CanCreate(a, b) == true ? "можно составить" : "нельзя составить";
+//Console.WriteLine(ans);
+////Console.WriteLine(test[0]);
+//Console.WriteLine("Введите cтроку для суммирования цифр в ней");
+//StringBuilder test = new StringBuilder(Console.ReadLine(), 50);
+//Console.WriteLine(ProcessingString.SumOfNumbersInString(test.ToString()));
+
+
+string test = "f(x)=cos(x)*sin(x)";
+char[] charSeparators = new char[] { '=', '+', '-', '/', '*' };
+string[] testArr = test.Split(charSeparators);
+string[] strSeparators = new string[] {"1", "2", "3", "4," };
+
+foreach(string el in testArr)
+{
+    Console.WriteLine(el);
+}
+
+Console.ReadLine();
---
+++ ProcessingString/ProcessingString.cs
@@ -0,0 +1,61 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Text.RegularExpressions;
+
+namespace ProcessingStringNamespace;
+public class ProcessingString
+{
+    public static bool CanCreate(string? s1, string? s2)
+    {
+        if (s1 == null || s2 == null)
+            return false;
+        char[] copies = new char[s1.Length];
+        int[] counters = new int[s1.Length];
+        int k = 0, arrInd;
+        for (int i = 0; i < s1.Length; i++)
+        {
+            arrInd = Array.IndexOf(copies, s1[i]);
+            if (arrInd == -1)
+            {
+                copies[k] = s1[i];
+                counters[k]++;
+                k++;
+            }
+            else
+            {
+                counters[arrInd]++;
+            }
+
+        }
+
+        for (int i = 0; i < s2.Length; i++)
+        {
+            arrInd = Array.IndexOf(copies, s2[i]);
+            if (arrInd == -1)
+            {
+                return false;
+            }
+            else if(counters[arrInd] <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                counters[arrInd]--;
+            }
+        }
+        return true;
+    }
+
+    public static string SumOfNumbersInString(String str)
+    {
+        Regex rx = new Regex(@"\d+");
+        int sum = 0;
+        MatchCollection mtchCllctn = rx.Matches(str);
+        foreach (Match digit in mtchCllctn)
+        {
+            sum += Convert.ToInt32(digit.Value);
+        }
+        return "sum of numbers = " + sum;
+    }
+}
+
